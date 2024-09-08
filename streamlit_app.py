e53e7782186049d751fbc82b0e18f2493253c6ab
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-09-08 15:24:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,13 @@ uploaded_file = st.file_uploader("Choose a png or jpg file", type=['png', 'jpg']
 col1, col2 = st.columns(2)
 with col1:
     st.header("Image Uploaded")
-    plt.subplot(1, 2, 1) 
-    plt.imshow(uploaded_file) 
+    if uploaded_file is not None:
+        # Read the image file
+        image = Image.open(uploaded_file)
+        # Display the uploaded image
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+    else:
+        st.write("Please upload an image")
 
 
 with col2:
