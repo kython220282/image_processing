bcdc8ee442029c9c83f1eeed9e92a751f56fa247
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-09-08 14:58:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import cv2 
+import matplotlib.pyplot as plt 
+import numpy as np 
 
-st.title('🎈 App Name')
+st.title('🎈 Image Processing Application')
 
 st.write('Hello world!')
