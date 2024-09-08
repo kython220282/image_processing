d216c1fc199627f84bb5720ee460a427eeb8aecb
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-09-08 15:17:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write('----------------')
 
 uploaded_file = st.file_uploader("Choose a png or jpg file", type=['png', 'jpg'] )
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 with col1:
     st.header("A cat")
     st.image("https://static.streamlit.io/examples/cat.jpg")
@@ -18,7 +18,3 @@ with col1:
 with col2:
     st.header("A dog")
     st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
