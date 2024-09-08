d83df48dccf161beec9cbc934cebbbe4a0524700
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-09-08 15:20:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
 st.title('🎈 Image Processing Application')
 
@@ -14,8 +14,10 @@ uploaded_file = st.file_uploader("Choose a png or jpg file", type=['png', 'jpg']
 
 col1, col2 = st.columns(2)
 with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
+    st.header("Image Uploaded")
+    plt.subplot(1, 2, 1) 
+    plt.imshow(uploaded_file) 
+
 
 with col2:
     st.header("A dog")
