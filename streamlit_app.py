a6b99abb8ac0ab715bba79bbbde2c6a1b902197b
Karan Raj Sharma <71350839+kython220282@users.noreply.github.com>
2024-09-08 15:13:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,20 @@ import numpy as np
 
 st.title('🎈 Image Processing Application')
 
-st.write('Hello world!')
+st.write('Open source application designed to different Image processing task')
+st.write('----------------')
+
+uploaded_file = st.file_uploader("Choose a png or jpg file", type=['png', 'jpg'] )
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
